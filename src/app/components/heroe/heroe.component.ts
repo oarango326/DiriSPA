1683d44028da0e685bd568d27c485a93340bf7d0
Oscar Arango <oscar.arango326@gmail.com>
2019-05-08 23:14:29 +0200

    componente heroe creado

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {HeroesService} from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroe.component.css']
 })
 export class HeroeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  heroe: any = [];
+  constructor( private activatedRoute: ActivatedRoute,
+               private _heroesService: HeroesService) {
+          this.activatedRoute.params.subscribe(params => {
+          this.heroe = this._heroesService.getHeroe(params.id);
+          console.log(this.heroe);
+    });
   }
-
+ ngOnInit() {
+  }
 }
